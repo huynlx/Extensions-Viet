119011efc6707ff6e3e24375e8920a87b1754b0b
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-07 20:27:30 +0700

    Updates

--- NetTruyen/source.js
+++ NetTruyen/source.js
@@ -544,7 +544,7 @@ class NetTruyen extends paperback_extensions_common_1.Source {
             data = yield this.requestManager.schedule(request, 1);
             $ = this.cheerio.load(data.data);
             let newAddedItems = [];
-            for (let manga of $('div.item', 'div.Module-163').toArray()) {
+            for (let manga of $('div.item', 'div.Module-170').toArray()) {
                 const title = $('figure.clearfix > figcaption > h3 > a', manga).first().text();
                 const id = (_c = $('figure.clearfix > div.image > a', manga).attr('href')) === null || _c === void 0 ? void 0 : _c.split('/').pop();
                 const image = $('figure.clearfix > div.image > a > img', manga).first().attr('data-original');
