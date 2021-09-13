85b83ac1bd93e74e4051cfd60ae02d57a992e8ea
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-13 18:10:39 +0700

    Updates

--- HentaiVN/source.js
+++ HentaiVN/source.js
@@ -821,13 +821,13 @@ class HentaiVN extends paperback_extensions_common_1.Source {
         });
     }
     getSearchResults(query, metadata) {
-        var _a;
+        var _a, _b, _c;
         return __awaiter(this, void 0, void 0, function* () {
             let page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1;
             const keyword = HentaiVNParser_1.generateSearch(query);
-            // const tag = query.includedTags?.map(tag => tag.id) ?? [];
+            const tag = (_c = (_b = query.includedTags) === null || _b === void 0 ? void 0 : _b.map(tag => tag.id)) !== null && _c !== void 0 ? _c : [];
             const request = createRequestObject({
-                url: `https://hentaivn.tv/tim-kiem-truyen.html?key=${keyword}`,
+                url: query.title ? (`https://hentaivn.tv/tim-kiem-truyen.html?key=${keyword}`) : (`https://hentaivn.tv${tag[0]}`),
                 method,
                 param: `&page=${page}`
             });
