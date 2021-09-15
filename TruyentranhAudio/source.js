da94715e746a780c0a22c6395550d98db211fb88
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-15 09:33:34 +0700

    Updates

--- TruyentranhAudio/source.js
+++ TruyentranhAudio/source.js
@@ -717,7 +717,7 @@ class TruyentranhAudio extends paperback_extensions_common_1.Source {
     getMangaDetails(mangaId) {
         var _a, _b;
         return __awaiter(this, void 0, void 0, function* () {
-            const url = `http://www.nettruyenpro.com/truyen-tranh/thuong-nhan-thanh-than-40970`;
+            const url = `${DOMAIN}truyen-tranh/${mangaId}`;
             const request = createRequestObject({
                 url: url,
                 method: "GET",
