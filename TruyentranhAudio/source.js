fc384a072222267e8e1a4e9566c9314783a3a690
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-15 09:20:46 +0700

    Updates

--- TruyentranhAudio/source.js
+++ TruyentranhAudio/source.js
@@ -754,20 +754,21 @@ class TruyentranhAudio extends paperback_extensions_common_1.Source {
         });
     }
     getChapters(mangaId) {
+        var _a;
         return __awaiter(this, void 0, void 0, function* () {
             const request = createRequestObject({
-                url: `https://truyentranhaudio.com/${mangaId}`,
+                url: `http://truyenqqtop.com/truyen-tranh/toi-luyen-thanh-than-11363`,
                 method,
             });
             const response = yield this.requestManager.schedule(request, 1);
             const $ = this.cheerio.load(response.data);
             const chapters = [];
-            for (const obj of $(".list-chapters > li").toArray().reverse()) {
+            for (const obj of $(".works-chapter-list > .works-chapter-item").toArray().reverse()) {
                 // if (!obj.attribs['href'] || !obj.children[0].data) continue;
                 chapters.push(createChapter({
-                    id: $('a', obj).attr('href'),
-                    chapNum: parseFloat($('a > .chapter-name', obj).text().split(' ')[1]),
-                    name: $('a > .chapter-name', obj).text(),
+                    id: (_a = $('.col-md-10.col-sm-10.col-xs-8 > a', obj).attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').pop(),
+                    chapNum: parseFloat($('.col-md-10.col-sm-10.col-xs-8 > a', obj).text().split(' ')[1]),
+                    name: $('.col-md-10.col-sm-10.col-xs-8 > a', obj).text(),
                     mangaId: mangaId,
                     langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
                 }));
