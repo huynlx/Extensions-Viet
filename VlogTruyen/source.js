ce058c7a9a9a6391eac4da7ea38ba36b4aa67551
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-25 21:31:15 +0700

    Updates

--- VlogTruyen/source.js
+++ VlogTruyen/source.js
@@ -907,7 +907,7 @@ class VlogTruyen extends paperback_extensions_common_1.Source {
                 tags.push({ id: id, label: label });
             }
             //nhom dich
-            for (const tag of $('select[name="translator"] > option:not(:first-child)').toArray()) {
+            for (const tag of $('#searchCate[name="translator"] > option:not(:first-child)').toArray()) {
                 const label = $(tag).text().trim();
                 const id = $(tag).attr('value');
                 if (!id || !label)
@@ -915,7 +915,7 @@ class VlogTruyen extends paperback_extensions_common_1.Source {
                 tags3.push({ id: id, label: label });
             }
             //tac gia
-            for (const tag of $('select[name="writer"] > option:not(:first-child)').toArray()) {
+            for (const tag of $('#searchCate[name="writer"] > option:not(:first-child)').toArray()) {
                 const label = $(tag).text().trim();
                 const id = $(tag).attr('value');
                 if (!id || !label)
@@ -923,7 +923,7 @@ class VlogTruyen extends paperback_extensions_common_1.Source {
                 tags4.push({ id: id, label: label });
             }
             //trang thai
-            for (const tag of $('select[name="status"] > option:not(:first-child)').toArray()) {
+            for (const tag of $('#searchCate[name="status"] > option:not(:first-child)').toArray()) {
                 const label = $(tag).text().trim();
                 const id = $(tag).attr('value');
                 if (!id || !label)
@@ -931,7 +931,7 @@ class VlogTruyen extends paperback_extensions_common_1.Source {
                 tags5.push({ id: id, label: label });
             }
             //sap xep
-            for (const tag of $('select[name="sort"] > option:not(:first-child)').toArray()) {
+            for (const tag of $('#searchCate[name="sort"] > option:not(:first-child)').toArray()) {
                 const label = $(tag).text().trim();
                 const id = $(tag).attr('value');
                 if (!id || !label)
