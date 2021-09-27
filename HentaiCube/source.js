62f0aea9a58af31f0ea2c51dd3ffd3668f16ecf4
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-27 23:51:07 +0700

    Updates

--- HentaiCube/source.js
+++ HentaiCube/source.js
@@ -670,15 +670,15 @@ class HentaiCube extends paperback_extensions_common_1.Source {
             var i = 0;
             for (const obj of $(".listing-chapters_wrap li").toArray().reverse()) {
                 i++;
-                // const getTime = $('span', obj).text().trim().split(/\//);
-                // const fixDate = [getTime[1], getTime[0], getTime[2]].join('/');
-                // const finalTime = new Date(fixDate);
+                const getTime = $('span', obj).text().trim();
+                let timeFinal = HentaiCubeParser_1.convertTime(getTime);
                 chapters.push(createChapter({
                     id: $('a', obj).first().attr('href'),
                     chapNum: i,
                     name: ($('a', obj).first().text().trim()),
                     mangaId: mangaId,
                     langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
+                    time: timeFinal
                 }));
             }
             return chapters;
@@ -990,7 +990,7 @@ class HentaiCube extends paperback_extensions_common_1.Source {
             var url = '';
             var set = 1;
             if (tags[0].split('.')[0] === 'year' || tags[0].split('.')[0] === 'sort') {
-                if (year && sort) {
+                if (year.length !== 0 && sort.length !== 0) {
                     url = encodeURI(`${year[0]}page/${page}/?${sort[0]}`);
                 }
                 else {
@@ -1096,7 +1096,7 @@ exports.HentaiCube = HentaiCube;
 },{"./HentaiCubeParser":56,"paperback-extensions-common":12}],56:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = exports.capitalizeFirstLetter = void 0;
+exports.convertTime = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = exports.capitalizeFirstLetter = void 0;
 const entities = require("entities"); //Import package for decoding HTML entities
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -1199,6 +1199,42 @@ exports.isLastPage = ($) => {
 };
 const decodeHTMLEntity = (str) => {
     return entities.decodeHTML(str);
+};
+exports.convertTime = (timeAgo) => {
+    var _a;
+    let time;
+    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
+    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
+    if (timeAgo.includes('giây') || timeAgo.includes('secs')) {
+        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
+    }
+    else if (timeAgo.includes('phút')) {
+        time = new Date(Date.now() - trimmed * 60000);
+    }
+    else if (timeAgo.includes('giờ')) {
+        time = new Date(Date.now() - trimmed * 3600000);
+    }
+    else if (timeAgo.includes('ngày')) {
+        time = new Date(Date.now() - trimmed * 86400000);
+    }
+    else if (timeAgo.includes('năm')) {
+        time = new Date(Date.now() - trimmed * 31556952000);
+    }
+    else {
+        if (timeAgo.includes(":")) {
+            let split = timeAgo.split(' ');
+            let H = split[0]; //vd => 21:08
+            let D = split[1]; //vd => 25/08 
+            let fixD = D.split('/');
+            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
+            time = new Date(finalD + ' ' + H);
+        }
+        else {
+            let split = timeAgo.split('/'); //vd => 05/12/18
+            time = new Date(split[1] + '/' + split[0] + '/' + split[2]);
+        }
+    }
+    return time;
 };
 
 },{"entities":1}]},{},[55])(55)
