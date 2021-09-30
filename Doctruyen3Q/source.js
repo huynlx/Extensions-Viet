e0553f9c04261aa0d58329dc4087d7b747458bd8
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-09-30 23:00:16 +0700

    Updates

--- Doctruyen3Q/source.js
+++ Doctruyen3Q/source.js
@@ -786,19 +786,12 @@ class Doctruyen3Q extends paperback_extensions_common_1.Source {
         let newUpdatedItems = [];
         for (const element of $('#home > .body > .main-left .item-manga > .item').toArray()) {
             let title = $('.caption > h3 > a', element).text().trim();
-            let img = $('.image-item > a > img', element).attr('data-origina');
-            let imageFinal = '';
-            if (typeof img !== 'undefined' && img !== false) {
-                imageFinal = img.attr("data-original");
-            }
-            else {
-                imageFinal = img.attr('src');
-            }
+            let img = $('.image-item > a > img', element).attr('data-original');
             let id = (_b = $('.caption > h3 > a', element).attr('href')) !== null && _b !== void 0 ? _b : title;
             let subtitle = $("ul > li:first-child > a", element).text().trim();
             newUpdatedItems.push(createMangaTile({
                 id: id !== null && id !== void 0 ? id : "",
-                image: imageFinal !== null && imageFinal !== void 0 ? imageFinal : "",
+                image: img !== null && img !== void 0 ? img : "",
                 title: createIconText({ text: title }),
                 subtitleText: createIconText({ text: subtitle }),
             }));
