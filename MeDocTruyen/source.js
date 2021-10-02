1651d1016027b3b46631cd038b01cfe6469de249
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-02 18:34:28 +0700

    Updates

--- MeDocTruyen/source.js
+++ MeDocTruyen/source.js
@@ -587,7 +587,7 @@ const paperback_extensions_common_1 = require("paperback-extensions-common");
 const MeDocTruyenParser_1 = require("./MeDocTruyenParser");
 const method = 'GET';
 exports.MeDocTruyenInfo = {
-    version: '2.0.0',
+    version: '2.5.0',
     name: 'MeDocTruyen',
     icon: 'icon.png',
     author: 'Huynhzip3',
@@ -609,42 +609,6 @@ class MeDocTruyen extends paperback_extensions_common_1.Source {
             requestsPerSecond: 5,
             requestTimeout: 20000
         });
-    }
-    convertTime(timeAgo) {
-        var _a;
-        let time;
-        let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-        trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-        if (timeAgo.includes('giây') || timeAgo.includes('secs')) {
-            time = new Date(Date.now() - trimmed * 1000);
-        }
-        else if (timeAgo.includes('phút')) {
-            time = new Date(Date.now() - trimmed * 60000);
-        }
-        else if (timeAgo.includes('giờ')) {
-            time = new Date(Date.now() - trimmed * 3600000);
-        }
-        else if (timeAgo.includes('ngày')) {
-            time = new Date(Date.now() - trimmed * 86400000);
-        }
-        else if (timeAgo.includes('năm')) {
-            time = new Date(Date.now() - trimmed * 31556952000);
-        }
-        else {
-            if (timeAgo.includes(":")) {
-                let split = timeAgo.split(' ');
-                let H = split[0];
-                let D = split[1];
-                let fixD = D.split('/');
-                let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-                time = new Date(finalD + ' ' + H);
-            }
-            else {
-                let split = timeAgo.split('/');
-                time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
-            }
-        }
-        return time;
     }
     getMangaShareUrl(mangaId) { return (mangaId); }
     ;
