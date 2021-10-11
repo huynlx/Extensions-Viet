2fa0ef098672aac10f4b4c65fbea10f30076bd9e
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-11 22:10:22 +0700

    Updates

--- CManga/source.js
+++ CManga/source.js
@@ -9439,173 +9439,6 @@ __exportStar(require("./SearchField"), exports);
 __exportStar(require("./RawData"), exports);
 
 },{"./Chapter":53,"./ChapterDetails":52,"./Constants":54,"./DynamicUI":70,"./HomeSection":71,"./Languages":72,"./Manga":75,"./MangaTile":73,"./MangaUpdate":74,"./PagedResults":76,"./RawData":77,"./RequestHeaders":78,"./RequestInterceptor":79,"./RequestManager":80,"./RequestObject":81,"./ResponseObject":82,"./SearchField":83,"./SearchRequest":84,"./SourceInfo":85,"./SourceManga":86,"./SourceStateManager":87,"./SourceTag":88,"./TagSection":89,"./TrackedManga":91,"./TrackedMangaChapterReadAction":90,"./TrackerActionQueue":92}],94:[function(require,module,exports){
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.titleCase = exports.decrypt_data = exports.convertTime = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
-const entities = require("entities"); //Import package for decoding HTML entities
-const DOMAIN = 'https://cmangatop.com/';
-exports.generateSearch = (query) => {
-    var _a;
-    let keyword = (_a = query.title) !== null && _a !== void 0 ? _a : "";
-    return encodeURI(keyword);
-};
-exports.parseSearch = ($) => {
-    const manga = [];
-    const collectedIds = [];
-    for (let obj of $('.thumb-item-flow', '.col-md-8 > .card:nth-child(2) .row-last-update').toArray()) {
-        const title = $('.series-title', obj).text().trim();
-        const id = $('.series-title > a', obj).attr('href');
-        const image = $('.thumb-wrapper > a > .a6-ratio > .img-in-ratio', obj).attr('data-bg');
-        const sub = $('.chapter-title  > a', obj).text().trim();
-        if (!id || !title)
-            continue;
-        if (!collectedIds.includes(id)) {
-            manga.push(createMangaTile({
-                id: id,
-                image: (image === null || image === void 0 ? void 0 : image.includes('http')) ? (image) : ((image === null || image === void 0 ? void 0 : image.includes('app')) ? (DOMAIN + image) : ('https:' + image)),
-                title: createIconText({
-                    text: title,
-                }),
-                subtitleText: createIconText({
-                    text: "Chương " + sub,
-                }),
-            }));
-            collectedIds.push(id);
-        }
-    }
-    return manga;
-};
-exports.parseViewMore = ($) => {
-    const manga = [];
-    const collectedIds = [];
-    for (let obj of $('.thumb-item-flow', '.col-md-8 > .card:nth-child(2) .row-last-update').toArray()) {
-        const title = $('.series-title', obj).text().trim();
-        const id = $('.series-title > a', obj).attr('href');
-        const image = $('.thumb-wrapper > a > .a6-ratio > .img-in-ratio', obj).attr('data-bg');
-        const sub = $('.chapter-title  > a', obj).text().trim();
-        if (!id || !title)
-            continue;
-        if (!collectedIds.includes(id)) {
-            manga.push(createMangaTile({
-                id: id,
-                image: (image === null || image === void 0 ? void 0 : image.includes('http')) ? (image) : ((image === null || image === void 0 ? void 0 : image.includes('app')) ? (DOMAIN + image) : ('https:' + image)),
-                title: createIconText({
-                    text: title,
-                }),
-                subtitleText: createIconText({
-                    text: "Chương " + sub,
-                }),
-            }));
-            collectedIds.push(id);
-        }
-    }
-    return manga;
-};
-// export const parseTags = ($: CheerioStatic): TagSection[] => {
-//     const arrayTags: Tag[] = [];
-//     for (const obj of $("li", "ul").toArray()) {
-//         const label = ($("a", obj).text().trim());
-//         const id = $('a', obj).attr('href') ?? "";
-//         if (id == "") continue;
-//         arrayTags.push({
-//             id: id,
-//             label: label,
-//         });
-//     }
-//     const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'Thể Loại', tags: arrayTags.map(x => createTag(x)) })];
-//     return tagSections;
-// }
-exports.isLastPage = ($) => {
-    let isLast = false;
-    const pages = [];
-    for (const page of $("li", "ul.pagination").toArray()) {
-        const p = Number($('a', page).text().trim());
-        if (isNaN(p))
-            continue;
-        pages.push(p);
-    }
-    const lastPage = Math.max(...pages);
-    const currentPage = Number($("li > a.active").text().trim());
-    if (currentPage >= lastPage)
-        isLast = true;
-    return isLast;
-};
-const decodeHTMLEntity = (str) => {
-    return entities.decodeHTML(str);
-};
-function convertTime(timeAgo) {
-    var _a;
-    let time;
-    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-    if (timeAgo.includes('giây')) {
-        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
-    }
-    else if (timeAgo.includes('phút')) {
-        time = new Date(Date.now() - trimmed * 60000);
-    }
-    else if (timeAgo.includes('giờ')) {
-        time = new Date(Date.now() - trimmed * 3600000);
-    }
-    else if (timeAgo.includes('ngày')) {
-        time = new Date(Date.now() - trimmed * 86400000);
-    }
-    else if (timeAgo.includes('tuần')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7);
-    }
-    else if (timeAgo.includes('tháng')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
-    }
-    else if (timeAgo.includes('năm')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
-    }
-    else {
-        if (timeAgo.includes(":")) {
-            let split = timeAgo.split(' ');
-            let H = split[0]; //vd => 21:08
-            let D = split[1]; //vd => 25/08 
-            let fixD = D.split('/');
-            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-            time = new Date(finalD + ' ' + H);
-        }
-        else {
-            let split = timeAgo.split('/'); //vd => 05/12/18
-            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
-        }
-    }
-    return time;
-}
-exports.convertTime = convertTime;
-function decrypt_data(data) {
-    const CryptoJS = require('crypto-js');
-    var parsed = (data);
-    var type = parsed.ciphertext;
-    var score = CryptoJS.enc.Hex.parse(parsed.iv);
-    var lastviewmatrix = CryptoJS.enc.Hex.parse(parsed.salt);
-    var adjustedLevel = CryptoJS.PBKDF2("nettruyenhayvn", lastviewmatrix, {
-        "hasher": CryptoJS.algo.SHA512,
-        "keySize": 64 / 8,
-        "iterations": 999
-    });
-    var queryTokenScores = { iv: '' };
-    queryTokenScores["iv"] = score;
-    var pixelSizeTargetMax = CryptoJS.AES.decrypt(type, adjustedLevel, queryTokenScores);
-    return pixelSizeTargetMax.toString(CryptoJS.enc.Utf8);
-}
-exports.decrypt_data = decrypt_data;
-function titleCase(str) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    // Directly return the joined string
-    return splitStr.join(' ');
-}
-exports.titleCase = titleCase;
-
-},{"crypto-js":13,"entities":39}],95:[function(require,module,exports){
 (function (Buffer){(function (){
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
@@ -9934,5 +9767,172 @@ class CManga extends paperback_extensions_common_1.Source {
 exports.CManga = CManga;
 
 }).call(this)}).call(this,require("buffer").Buffer)
-},{"./CMangaParser":94,"buffer":3,"paperback-extensions-common":51}]},{},[95])(95)
+},{"./CMangaParser":95,"buffer":3,"paperback-extensions-common":51}],95:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.titleCase = exports.decrypt_data = exports.convertTime = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
+const entities = require("entities"); //Import package for decoding HTML entities
+const DOMAIN = 'https://cmangatop.com/';
+exports.generateSearch = (query) => {
+    var _a;
+    let keyword = (_a = query.title) !== null && _a !== void 0 ? _a : "";
+    return encodeURI(keyword);
+};
+exports.parseSearch = ($) => {
+    const manga = [];
+    const collectedIds = [];
+    for (let obj of $('.thumb-item-flow', '.col-md-8 > .card:nth-child(2) .row-last-update').toArray()) {
+        const title = $('.series-title', obj).text().trim();
+        const id = $('.series-title > a', obj).attr('href');
+        const image = $('.thumb-wrapper > a > .a6-ratio > .img-in-ratio', obj).attr('data-bg');
+        const sub = $('.chapter-title  > a', obj).text().trim();
+        if (!id || !title)
+            continue;
+        if (!collectedIds.includes(id)) {
+            manga.push(createMangaTile({
+                id: id,
+                image: (image === null || image === void 0 ? void 0 : image.includes('http')) ? (image) : ((image === null || image === void 0 ? void 0 : image.includes('app')) ? (DOMAIN + image) : ('https:' + image)),
+                title: createIconText({
+                    text: title,
+                }),
+                subtitleText: createIconText({
+                    text: "Chương " + sub,
+                }),
+            }));
+            collectedIds.push(id);
+        }
+    }
+    return manga;
+};
+exports.parseViewMore = ($) => {
+    const manga = [];
+    const collectedIds = [];
+    for (let obj of $('.thumb-item-flow', '.col-md-8 > .card:nth-child(2) .row-last-update').toArray()) {
+        const title = $('.series-title', obj).text().trim();
+        const id = $('.series-title > a', obj).attr('href');
+        const image = $('.thumb-wrapper > a > .a6-ratio > .img-in-ratio', obj).attr('data-bg');
+        const sub = $('.chapter-title  > a', obj).text().trim();
+        if (!id || !title)
+            continue;
+        if (!collectedIds.includes(id)) {
+            manga.push(createMangaTile({
+                id: id,
+                image: (image === null || image === void 0 ? void 0 : image.includes('http')) ? (image) : ((image === null || image === void 0 ? void 0 : image.includes('app')) ? (DOMAIN + image) : ('https:' + image)),
+                title: createIconText({
+                    text: title,
+                }),
+                subtitleText: createIconText({
+                    text: "Chương " + sub,
+                }),
+            }));
+            collectedIds.push(id);
+        }
+    }
+    return manga;
+};
+// export const parseTags = ($: CheerioStatic): TagSection[] => {
+//     const arrayTags: Tag[] = [];
+//     for (const obj of $("li", "ul").toArray()) {
+//         const label = ($("a", obj).text().trim());
+//         const id = $('a', obj).attr('href') ?? "";
+//         if (id == "") continue;
+//         arrayTags.push({
+//             id: id,
+//             label: label,
+//         });
+//     }
+//     const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'Thể Loại', tags: arrayTags.map(x => createTag(x)) })];
+//     return tagSections;
+// }
+exports.isLastPage = ($) => {
+    let isLast = false;
+    const pages = [];
+    for (const page of $("li", "ul.pagination").toArray()) {
+        const p = Number($('a', page).text().trim());
+        if (isNaN(p))
+            continue;
+        pages.push(p);
+    }
+    const lastPage = Math.max(...pages);
+    const currentPage = Number($("li > a.active").text().trim());
+    if (currentPage >= lastPage)
+        isLast = true;
+    return isLast;
+};
+const decodeHTMLEntity = (str) => {
+    return entities.decodeHTML(str);
+};
+function convertTime(timeAgo) {
+    var _a;
+    let time;
+    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
+    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
+    if (timeAgo.includes('giây')) {
+        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
+    }
+    else if (timeAgo.includes('phút')) {
+        time = new Date(Date.now() - trimmed * 60000);
+    }
+    else if (timeAgo.includes('giờ')) {
+        time = new Date(Date.now() - trimmed * 3600000);
+    }
+    else if (timeAgo.includes('ngày')) {
+        time = new Date(Date.now() - trimmed * 86400000);
+    }
+    else if (timeAgo.includes('tuần')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7);
+    }
+    else if (timeAgo.includes('tháng')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
+    }
+    else if (timeAgo.includes('năm')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
+    }
+    else {
+        if (timeAgo.includes(":")) {
+            let split = timeAgo.split(' ');
+            let H = split[0]; //vd => 21:08
+            let D = split[1]; //vd => 25/08 
+            let fixD = D.split('/');
+            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
+            time = new Date(finalD + ' ' + H);
+        }
+        else {
+            let split = timeAgo.split('/'); //vd => 05/12/18
+            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
+        }
+    }
+    return time;
+}
+exports.convertTime = convertTime;
+function decrypt_data(data) {
+    const CryptoJS = require('crypto-js');
+    var parsed = (data);
+    var type = parsed.ciphertext;
+    var score = CryptoJS.enc.Hex.parse(parsed.iv);
+    var lastviewmatrix = CryptoJS.enc.Hex.parse(parsed.salt);
+    var adjustedLevel = CryptoJS.PBKDF2("nettruyenhayvn", lastviewmatrix, {
+        "hasher": CryptoJS.algo.SHA512,
+        "keySize": 64 / 8,
+        "iterations": 999
+    });
+    var queryTokenScores = { iv: '' };
+    queryTokenScores["iv"] = score;
+    var pixelSizeTargetMax = CryptoJS.AES.decrypt(type, adjustedLevel, queryTokenScores);
+    return pixelSizeTargetMax.toString(CryptoJS.enc.Utf8);
+}
+exports.decrypt_data = decrypt_data;
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+}
+exports.titleCase = titleCase;
+
+},{"crypto-js":13,"entities":39}]},{},[94])(94)
 });
