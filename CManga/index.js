41a47c54d1ef55da37ab2e6e4550d2fe84c3a63b
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-13 01:00:40 +0700

    Updates

--- CManga/index.js
+++ CManga/index.js
@@ -7435,7 +7435,7 @@ const CMangaParser_1 = require("./CMangaParser");
 const DOMAIN = 'https://cmangatop.com/';
 const method = 'GET';
 exports.CMangaInfo = {
-    version: '1.0.0',
+    version: '2.0.0',
     name: 'CManga',
     icon: 'icon.png',
     author: 'Huynhzip3',
@@ -7500,13 +7500,6 @@ class CManga extends paperback_extensions_common_1.Source {
     }
     getChapters(mangaId) {
         return __awaiter(this, void 0, void 0, function* () {
-            // const request = createRequestObject({
-            //     url: DOMAIN + mangaId.split("::")[0],
-            //     method,
-            // });
-            // const response = await this.requestManager.schedule(request, 1);
-            // const $ = this.cheerio.load(response.data);
-            // const book_id = $.html().match(/book_id.+"(.+)"/)[1];
             const request2 = createRequestObject({
                 url: "https://cmangatop.com/api/book_chapter?opt1=" + mangaId.split("::")[2],
                 method: "GET",
@@ -7657,7 +7650,7 @@ class CManga extends paperback_extensions_common_1.Source {
     getViewMoreItems(homepageSectionId, metadata) {
         var _a;
         return __awaiter(this, void 0, void 0, function* () {
-            let page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1;
+            let page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1000;
             let param = '';
             let url = '';
             let method = "GET";
@@ -7833,7 +7826,7 @@ exports.CManga = CManga;
 },{"./CMangaParser":92,"paperback-extensions-common":48}],92:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.change_alias = exports.titleCase = exports.decrypt_data = exports.convertTime = exports.decodeHTMLEntity = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
+exports.change_alias = exports.titleCase = exports.decrypt_data = exports.decodeHTMLEntity = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
 const entities = require("entities"); //Import package for decoding HTML entities
 const DOMAIN = 'https://cmangatop.com/';
 exports.generateSearch = (query) => {
@@ -7900,81 +7893,9 @@ exports.parseViewMore = (json) => {
     }
     return manga;
 };
-// export const parseTags = ($: CheerioStatic): TagSection[] => {
-//     const arrayTags: Tag[] = [];
-//     for (const obj of $("li", "ul").toArray()) {
-//         const label = ($("a", obj).text().trim());
-//         const id = $('a', obj).attr('href') ?? "";
-//         if (id == "") continue;
-//         arrayTags.push({
-//             id: id,
-//             label: label,
-//         });
-//     }
-//     const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'Thể Loại', tags: arrayTags.map(x => createTag(x)) })];
-//     return tagSections;
-// }
-exports.isLastPage = ($) => {
-    let isLast = false;
-    const pages = [];
-    for (const page of $("li", "ul.pagination").toArray()) {
-        const p = Number($('a', page).text().trim());
-        if (isNaN(p))
-            continue;
-        pages.push(p);
-    }
-    const lastPage = Math.max(...pages);
-    const currentPage = Number($("li > a.active").text().trim());
-    if (currentPage >= lastPage)
-        isLast = true;
-    return isLast;
-};
 exports.decodeHTMLEntity = (str) => {
     return entities.decodeHTML(str);
 };
-function convertTime(timeAgo) {
-    var _a;
-    let time;
-    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-    if (timeAgo.includes('giây')) {
-        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
-    }
-    else if (timeAgo.includes('phút')) {
-        time = new Date(Date.now() - trimmed * 60000);
-    }
-    else if (timeAgo.includes('giờ')) {
-        time = new Date(Date.now() - trimmed * 3600000);
-    }
-    else if (timeAgo.includes('ngày')) {
-        time = new Date(Date.now() - trimmed * 86400000);
-    }
-    else if (timeAgo.includes('tuần')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7);
-    }
-    else if (timeAgo.includes('tháng')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
-    }
-    else if (timeAgo.includes('năm')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
-    }
-    else {
-        if (timeAgo.includes(":")) {
-            let split = timeAgo.split(' ');
-            let H = split[0]; //vd => 21:08
-            let D = split[1]; //vd => 25/08 
-            let fixD = D.split('/');
-            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-            time = new Date(finalD + ' ' + H);
-        }
-        else {
-            let split = timeAgo.split('/'); //vd => 05/12/18
-            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
-        }
-    }
-    return time;
-}
-exports.convertTime = convertTime;
 function decrypt_data(data) {
     const CryptoJS = require('crypto-js');
     var parsed = (data);
