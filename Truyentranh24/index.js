31479ac710acdf4a17299e93430727e9b26ce4c4
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-16 19:41:25 +0700

    Updates

--- Truyentranh24/index.js
+++ Truyentranh24/index.js
@@ -597,7 +597,7 @@ const Truyentranh24Parser_1 = require("./Truyentranh24Parser");
 const DOMAIN = 'https://truyentranh24.com/';
 const method = 'GET';
 exports.Truyentranh24Info = {
-    version: '1.0.0',
+    version: '1.5.0',
     name: 'Truyentranh24',
     icon: 'icon.png',
     author: 'Huynhzip3',
@@ -620,48 +620,6 @@ class Truyentranh24 extends paperback_extensions_common_1.Source {
             requestTimeout: 20000
         });
         this.dataId = '';
-    }
-    convertTime(timeAgo) {
-        var _a;
-        let time;
-        let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-        trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-        if (timeAgo.includes('giây') || timeAgo.includes('secs')) {
-            time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
-        }
-        else if (timeAgo.includes('phút')) {
-            time = new Date(Date.now() - trimmed * 60000);
-        }
-        else if (timeAgo.includes('giờ')) {
-            time = new Date(Date.now() - trimmed * 3600000);
-        }
-        else if (timeAgo.includes('ngày')) {
-            time = new Date(Date.now() - trimmed * 86400000);
-        }
-        else if (timeAgo.includes('tuần')) {
-            time = new Date(Date.now() - trimmed * 86400000 * 7);
-        }
-        else if (timeAgo.includes('tháng')) {
-            time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
-        }
-        else if (timeAgo.includes('năm')) {
-            time = new Date(Date.now() - trimmed * 31556952000);
-        }
-        else {
-            if (timeAgo.includes(":")) {
-                let split = timeAgo.split(' ');
-                let H = split[0]; //vd => 21:08
-                let D = split[1]; //vd => 25/08 
-                let fixD = D.split('/');
-                let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-                time = new Date(finalD + ' ' + H);
-            }
-            else {
-                let split = timeAgo.split('-'); //vd => 05/12/18
-                time = new Date(split[1] + '/' + split[0] + '/' + split[2]);
-            }
-        }
-        return time;
     }
     getMangaShareUrl(mangaId) { return (DOMAIN + mangaId); }
     ;
@@ -730,7 +688,6 @@ class Truyentranh24 extends paperback_extensions_common_1.Source {
                     time: new Date(d[1] + '/' + d[2] + '/' + d[0] + ' ' + t[0] + ':' + t[1])
                 }));
             }
-            console.log(chapters);
             return chapters;
         });
     }
