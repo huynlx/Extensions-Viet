d5f0858bfc024072a14e3ef2fc557440dc061eb1
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-19 20:06:09 +0700

    Updates

--- Truyendep/index.js
+++ Truyendep/index.js
@@ -1016,7 +1016,8 @@ class Truyendep extends paperback_extensions_common_1.Source {
             let page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1;
             const tags = (_c = (_b = query.includedTags) === null || _b === void 0 ? void 0 : _b.map(tag => tag.id)) !== null && _c !== void 0 ? _c : [];
             const request = createRequestObject({
-                url: query.title ? (`https://truyendep.net/wp-content/themes/manga/list-manga-front.js?nocache=1634580614`) : `${tags[0]}/page/${page}/`,
+                url: query.title ? (`https://truyendep.net/wp-content/themes/manga/list-manga-front.js?nocache=1634580614`)
+                    : (tags[0].includes('tieudiem') ? DOMAIN : `${tags[0]}/page/${page}/`),
                 method: "GET",
             });
             const data = yield this.requestManager.schedule(request, 1);
@@ -1081,7 +1082,7 @@ class Truyendep extends paperback_extensions_common_1.Source {
             const $ = this.cheerio.load(response.data);
             const arrayTags = [{
                     label: 'Tiêu điểm',
-                    id: 'tieudiem::https://truyendep.net/'
+                    id: 'tieudiem'
                 }];
             const collectedIds = [];
             //the loai
