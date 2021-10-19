d4dd331c52a66009bbf5ae2ccdaaea7a915d1a1f
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-19 14:28:12 +0700

    Updates

--- Truyendep/source.js
+++ Truyendep/source.js
@@ -995,27 +995,44 @@ exports.parseSearch = ($) => {
     return mangas;
 };
 exports.parseViewMore = ($, select) => {
-    var _a, _b, _c, _d, _e;
+    var _a, _b, _c, _d, _e, _f;
     const mangas = [];
     if (select === 1) {
+        // for (let manga of $('.wrap_update .update_item').toArray()) {
+        //     const title = $('a', manga).first().attr('title');
+        //     const id = $('a', manga).first().attr('href') ?? title;
+        //     const image = $('.update_image img', manga).attr('src')?.replace('-61x61', '');
+        //     const sub = 'Chap' + $('a:nth-of-type(1)', manga).text().trim().split('chap')[1];
+        //     mangas.push(createMangaTile({
+        //         id: id ?? "",
+        //         image: image ?? "",
+        //         title: createIconText({ text: decodeHTMLEntity(title ?? "") }),
+        //         subtitleText: createIconText({ text: sub }),
+        //     }));
+        // }
         for (let manga of $('.wrap_update .update_item').toArray()) {
-            const title = $('a', manga).first().attr('title');
-            const id = (_a = $('a', manga).first().attr('href')) !== null && _a !== void 0 ? _a : title;
-            const image = (_b = $('.update_image img', manga).attr('src')) === null || _b === void 0 ? void 0 : _b.replace('-61x61', '');
-            const sub = 'Chap' + $('a:nth-of-type(1)', manga).text().trim().split('chap')[1];
+            const title = (_a = $('h3.nowrap a', manga).attr('title')) !== null && _a !== void 0 ? _a : "";
+            const id = (_b = $('h3.nowrap a', manga).attr('href')) !== null && _b !== void 0 ? _b : title;
+            const image = (_c = $('a img', manga).attr('src')) === null || _c === void 0 ? void 0 : _c.split('-');
+            const ext = image === null || image === void 0 ? void 0 : image.splice(-1)[0].split('.')[1];
+            const sub = 'Chap' + $('a', manga).last().text().trim().split('chap')[1];
             mangas.push(createMangaTile({
-                id: id !== null && id !== void 0 ? id : "",
-                image: image !== null && image !== void 0 ? image : "",
-                title: createIconText({ text: exports.decodeHTMLEntity(title !== null && title !== void 0 ? title : "") }),
-                subtitleText: createIconText({ text: sub }),
+                id: id,
+                image: (image === null || image === void 0 ? void 0 : image.join('-')) + '.' + ext,
+                title: createIconText({
+                    text: exports.decodeHTMLEntity(title),
+                }),
+                subtitleText: createIconText({
+                    text: sub,
+                }),
             }));
         }
     }
     else {
         for (let manga of $('.wrap_update .update_item').toArray()) {
-            const title = (_c = $('h3.nowrap a', manga).attr('title')) !== null && _c !== void 0 ? _c : "";
-            const id = (_d = $('h3.nowrap a', manga).attr('href')) !== null && _d !== void 0 ? _d : title;
-            const image = (_e = $('a img', manga).attr('src')) === null || _e === void 0 ? void 0 : _e.split('-');
+            const title = (_d = $('h3.nowrap a', manga).attr('title')) !== null && _d !== void 0 ? _d : "";
+            const id = (_e = $('h3.nowrap a', manga).attr('href')) !== null && _e !== void 0 ? _e : title;
+            const image = (_f = $('a img', manga).attr('src')) === null || _f === void 0 ? void 0 : _f.split('-');
             const ext = image === null || image === void 0 ? void 0 : image.splice(-1)[0].split('.')[1];
             const sub = 'Chap' + $('a', manga).last().text().trim().split('chap')[1];
             mangas.push(createMangaTile({
