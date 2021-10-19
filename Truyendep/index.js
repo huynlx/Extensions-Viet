ef17d3e7e01cc6710a7ac436e5736ce3a50fdd71
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-19 16:21:24 +0700

    Updates

--- Truyendep/index.js
+++ Truyendep/index.js
@@ -597,7 +597,7 @@ const TruyendepParser_1 = require("./TruyendepParser");
 const DOMAIN = 'https://truyendep.net/';
 const method = 'GET';
 exports.TruyendepInfo = {
-    version: '1.0.0',
+    version: '2.4.0',
     name: 'Truyendep',
     icon: 'icon.png',
     author: 'Huynhzip3',
@@ -685,7 +685,7 @@ class Truyendep extends paperback_extensions_common_1.Source {
             var i = 0;
             for (const obj of $(".chapter-list .row").toArray().reverse()) {
                 i++;
-                var y = $('span:first-child > a', obj).text();
+                var y = $('span:first-child', obj).text();
                 var chapNum = parseFloat(y.includes('chap') ? y.split('chap')[1].split(' ')[1] : (y.includes('Chap') ? y.split('Chap')[1].split(' ')[1] : y.split('Chương')[1].split(' ')[1]));
                 var time = $('span:last-child', obj).text().trim().split('-');
                 chapters.push(createChapter({
@@ -1061,9 +1061,8 @@ exports.Truyendep = Truyendep;
 },{"./TruyendepParser":56,"paperback-extensions-common":12}],56:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertTime = exports.decodeHTMLEntity = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
+exports.decodeHTMLEntity = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
 const entities = require("entities"); //Import package for decoding HTML entities
-const DOMAIN = 'https://truyentranhaudio.online/';
 exports.generateSearch = (query) => {
     var _a;
     let keyword = (_a = query.title) !== null && _a !== void 0 ? _a : "";
@@ -1130,20 +1129,6 @@ exports.parseViewMore = ($, select) => {
     }
     return mangas;
 };
-// export const parseTags = ($: CheerioStatic): TagSection[] => {
-//     const arrayTags: Tag[] = [];
-//     for (const obj of $("li", "ul").toArray()) {
-//         const label = ($("a", obj).text().trim());
-//         const id = $('a', obj).attr('href') ?? "";
-//         if (id == "") continue;
-//         arrayTags.push({
-//             id: id,
-//             label: label,
-//         });
-//     }
-//     const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'Thể Loại', tags: arrayTags.map(x => createTag(x)) })];
-//     return tagSections;
-// }
 exports.isLastPage = ($) => {
     let isLast = false;
     const pages = [];
@@ -1162,49 +1147,6 @@ exports.isLastPage = ($) => {
 exports.decodeHTMLEntity = (str) => {
     return entities.decodeHTML(str);
 };
-function convertTime(timeAgo) {
-    var _a;
-    let time;
-    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-    if (timeAgo.includes('giây')) {
-        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
-    }
-    else if (timeAgo.includes('phút')) {
-        time = new Date(Date.now() - trimmed * 60000);
-    }
-    else if (timeAgo.includes('giờ')) {
-        time = new Date(Date.now() - trimmed * 3600000);
-    }
-    else if (timeAgo.includes('ngày')) {
-        time = new Date(Date.now() - trimmed * 86400000);
-    }
-    else if (timeAgo.includes('tuần')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7);
-    }
-    else if (timeAgo.includes('tháng')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
-    }
-    else if (timeAgo.includes('năm')) {
-        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
-    }
-    else {
-        if (timeAgo.includes(":")) {
-            let split = timeAgo.split(' ');
-            let H = split[0]; //vd => 21:08
-            let D = split[1]; //vd => 25/08 
-            let fixD = D.split('/');
-            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-            time = new Date(finalD + ' ' + H);
-        }
-        else {
-            let split = timeAgo.split('/'); //vd => 05/12/18
-            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
-        }
-    }
-    return time;
-}
-exports.convertTime = convertTime;
 
 },{"entities":1}]},{},[55])(55)
 });
