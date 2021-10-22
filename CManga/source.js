dac77d0d4fd44204a3aff659b0fb6d393fe40407
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-10-22 11:28:26 +0700

    Updates

--- CManga/source.js
+++ CManga/source.js
@@ -7538,7 +7538,7 @@ class CManga extends paperback_extensions_common_1.Source {
             var pages = [];
             for (const img of chapter_content) {
                 // pages.push(img);
-                pages.push(img.replace('.net', '.com'));
+                pages.push(encodeURI(img).replace('.net', '.com'));
             }
             const chapterDetails = createChapterDetails({
                 id: chapterId,
