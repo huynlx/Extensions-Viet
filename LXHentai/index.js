9d7c8a39da55bace82148e6befe1b74536c0bc36
Lê Xuân Huynh <huynhlx62@wru.vn>
2024-09-29 18:48:54 +0700

    Updates

--- LXHentai/index.js
+++ LXHentai/index.js
@@ -2436,6 +2436,42 @@ class LXHentai extends paperback_extensions_common_1.Source {
             },
         });
     }
+    convertTime(timeAgo) {
+        var _a;
+        let time;
+        let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
+        trimmed = trimmed == 0 && timeAgo.includes("a") ? 1 : trimmed;
+        if (timeAgo.includes("giây") || timeAgo.includes("secs")) {
+            time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
+        }
+        else if (timeAgo.includes("phút")) {
+            time = new Date(Date.now() - trimmed * 60000);
+        }
+        else if (timeAgo.includes("giờ")) {
+            time = new Date(Date.now() - trimmed * 3600000);
+        }
+        else if (timeAgo.includes("ngày")) {
+            time = new Date(Date.now() - trimmed * 86400000);
+        }
+        else if (timeAgo.includes("năm")) {
+            time = new Date(Date.now() - trimmed * 31556952000);
+        }
+        else {
+            if (timeAgo.includes(":")) {
+                let split = timeAgo.split(" ");
+                let H = split[0]; //vd => 21:08
+                let D = split[1]; //vd => 25/08
+                let fixD = D.split("/");
+                let finalD = fixD[1] + "/" + fixD[0] + "/" + new Date().getFullYear();
+                time = new Date(finalD + " " + H);
+            }
+            else {
+                let split = timeAgo.split("/"); //vd => 05/12/18
+                time = new Date(split[1] + "/" + split[0] + "/" + "20" + split[2]);
+            }
+        }
+        return time;
+    }
     getMangaShareUrl(mangaId) {
         return `${mangaId}`;
     }
@@ -2467,7 +2503,7 @@ class LXHentai extends paperback_extensions_common_1.Source {
                 tags.push(createTag({ label: genre, id }));
             }
             artist = $("div:contains('Nhóm dịch:')").next().text();
-            const image = $("div.cover-frame").css("background-image");
+            const image = $("div.cover-frame > .cover").css("background-image");
             const bg = image === null || image === void 0 ? void 0 : image.replace("url(", "").replace(")", "").replace(/\"/gi, "").replace(/['"]+/g, "");
             return createManga({
                 id: mangaId,
@@ -2475,7 +2511,7 @@ class LXHentai extends paperback_extensions_common_1.Source {
                 artist: artist,
                 desc: desc,
                 titles: [$(".grow.text-lg.ml-1.text-ellipsis.font-semibold").text()],
-                image: bg,
+                image: bg || "",
                 status: status,
                 // rating: parseFloat($('span[itemprop="ratingValue"]').text()),
                 hentai: true,
@@ -2493,20 +2529,20 @@ class LXHentai extends paperback_extensions_common_1.Source {
             const $ = this.cheerio.load(response.data);
             const chapters = [];
             var i = 0;
-            for (const obj of $("#listChuong > ul > .row:not(:first-child) > div.col-5")
+            for (const obj of $("ul.overflow-y-auto.overflow-x-hidden > a")
                 .toArray()
                 .reverse()) {
                 i++;
-                let time = $($(obj).next()).text().trim().split(" ");
-                let day = time[1].split("/");
-                let h = time[0];
+                let time = $(".hidden > span", obj).last().text();
+                let view = $(".hidden > span", obj).first().text();
                 chapters.push(createChapter({
-                    id: "https://lxmanga.click" + $("a", obj).attr("href"),
+                    id: "https://lxmanga.click" + $(obj).attr("href"),
                     chapNum: i,
-                    name: $("a", obj).text(),
+                    name: $(".text-ellipsis", obj).text(),
                     mangaId: mangaId,
                     langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
-                    time: new Date(day[1] + "/" + day[0] + "/" + day[2] + " " + h),
+                    time: this.convertTime(time),
+                    group: view + " lượt xem",
                 }));
             }
             return chapters;
@@ -2521,13 +2557,9 @@ class LXHentai extends paperback_extensions_common_1.Source {
             const response = yield this.requestManager.schedule(request, 1);
             let $ = this.cheerio.load(response.data);
             const pages = [];
-            const list = $("#content_chap p img").toArray().length === 0
-                ? $("#content_chap div:not(.text-center) img").toArray()
-                : $("#content_chap p img").toArray();
+            const list = $(".text-center > #image-container").toArray();
             for (let obj of list) {
-                let link = obj.attribs["src"].includes("http")
-                    ? obj.attribs["src"]
-                    : "https:" + obj.attribs["src"];
+                let link = $("img", obj).last().attr("src");
                 pages.push(encodeURI(link));
             }
             const chapterDetails = createChapterDetails({
