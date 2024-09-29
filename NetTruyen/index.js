973a4499ebf5999fa589d156b40de699ec704348
Lê Xuân Huynh <huynhlx62@wru.vn>
2024-09-29 11:49:44 +0700

    Updates

--- NetTruyen/index.js
+++ NetTruyen/index.js
@@ -377,7 +377,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NetTruyen = exports.NetTruyenInfo = exports.isLastPage = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const NetTruyenParser_1 = require("./NetTruyenParser");
-const DOMAIN = "https://nettruyenww.com/";
+const DOMAIN = "https://www.nettruyenus.com/";
 exports.isLastPage = ($) => {
     const current = $("ul.pagination > li.active > a").text();
     let total = $("ul.pagination > li.PagerSSCCells:last-child").text();
@@ -388,7 +388,7 @@ exports.isLastPage = ($) => {
     return true;
 };
 exports.NetTruyenInfo = {
-    version: "3.0.5",
+    version: "3.0.4",
     name: "NetTruyen",
     icon: "icon.png",
     author: "Huynhzip3",
