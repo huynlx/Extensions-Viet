5e4a892b123372bf1ee1beda37fc94b14a2dc1c6
Lê Xuân Huynh <huynhlx62@wru.vn>
2024-09-30 18:23:47 +0700

    Updates

--- TruyenQQ/index.js
+++ TruyenQQ/index.js
@@ -597,7 +597,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TruyenQQ = exports.TruyenQQInfo = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const TruyenQQParser_1 = require("./TruyenQQParser");
-const DOMAIN = "https://truyenqqq.com/";
+const DOMAIN = "https://truyenqqto.com/";
 const method = "GET";
 exports.TruyenQQInfo = {
     version: "3.0.1",
